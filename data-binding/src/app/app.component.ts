290622853c391b0d10a29f48b8164dbcdd71a3e5
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-08-01 09:22:41 +0200

    megoldás

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  title: string = 'angular-practice-001';
+  getName(){
+    return 'Bond, James Bond';
+  };
 }
