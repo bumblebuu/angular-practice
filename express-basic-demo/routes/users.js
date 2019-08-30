15a333372ffd8813c0fb894017c05220311fc3c9
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-08-30 21:01:27 +0200

    mamamama

---
+++ express-basic-demo/routes/users.js
@@ -0,0 +1,13 @@
+let express = require('express');
+
+let router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.render('users', {
+    title: 'Users',
+    lead: 'This page shows a list of users.'
+  });
+});
+
+module.exports = router;
